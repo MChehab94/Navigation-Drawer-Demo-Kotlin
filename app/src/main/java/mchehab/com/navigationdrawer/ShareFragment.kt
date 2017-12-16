36ca765ce02ae942ce792bdb09374929dc664ea5
Mohamad <lemirmohammad.shehab@lau.edu>
2017-12-16 16:55:36 +0200

    Created a fragment for each menu item in drawer and their corresponding title in strings.xml

---
+++ app/src/main/java/mchehab/com/navigationdrawer/ShareFragment.kt
@@ -0,0 +1,16 @@
+package mchehab.com.navigationdrawer
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ShareFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_share, container, false)
+    }
+}// Required empty public constructor
---
+++ app/src/main/java/mchehab/com/navigationdrawer/ToolsFragment.kt
@@ -0,0 +1,19 @@
+package mchehab.com.navigationdrawer
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ToolsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_tools, container, false)
+    }
+
+}// Required empty public constructor
